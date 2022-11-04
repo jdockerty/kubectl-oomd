7d54f4f741190ad8a10bc684a67aea9ab36721f4
Jack Dockerty <jdockerty@influxdata.com>
2022-11-04 17:13:46 +0000

    feat: add iteration through pod statuses

--- pkg/plugin/plugin.go
+++ pkg/plugin/plugin.go
@@ -3,12 +3,21 @@ package plugin
 import (
 	"fmt"
 
+	"github.com/jdockerty/kubectl-oomlie/pkg/logger"
+	v1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/cli-runtime/pkg/genericclioptions"
 	"k8s.io/client-go/kubernetes"
 )
 
-func RunPlugin(configFlags *genericclioptions.ConfigFlags, outputCh chan string) error {
+type TerminatedPodInfo struct {
+	Pod            v1.Pod
+	ContainerName  string // Name of the container within the pod that was terminated, in the case of multi-container pods.
+	TerminatedTime string // When the pod was terminated
+	StartTime      string // When the pod was started during the termination period.
+}
+
+func RunPlugin(configFlags *genericclioptions.ConfigFlags, namespace string, logger *logger.Logger) error {
 	config, err := configFlags.ToRESTConfig()
 	if err != nil {
 		return fmt.Errorf("failed to read kubeconfig: %w", err)
@@ -19,13 +28,43 @@ func RunPlugin(configFlags *genericclioptions.ConfigFlags, outputCh chan string)
 		return fmt.Errorf("failed to create clientset: %w", err)
 	}
 
-	namespaces, err := clientset.CoreV1().Namespaces().List(metav1.ListOptions{})
-	if err != nil {
-		return fmt.Errorf("failed to list namespaces: %w", err)
+	if namespace == "" {
+		// Retrieve the current namespace from the raw kubeconfig struct
+		currentNamespace, _, err := configFlags.ToRawKubeConfigLoader().Namespace()
+		if err != nil {
+			return fmt.Errorf("failed to during creating raw kubeconfig: %w", err)
+		}
+		namespace = currentNamespace
 	}
 
-	for _, namespace := range namespaces.Items {
-		outputCh <- fmt.Sprintf("Namespace %s", namespace.Name)
+	pods, err := clientset.CoreV1().Pods(namespace).List(metav1.ListOptions{})
+	if err != nil {
+		return fmt.Errorf("failed to list pods: %w", err)
+	}
+
+	var terminatedPodsInfo []TerminatedPodInfo
+	for _, pod := range pods.Items {
+		for _, cStatus := range pod.Status.ContainerStatuses {
+
+			// The terminated state may be nil, i.e. not terminated, we must check this first.
+			if terminated := cStatus.LastTerminationState.Terminated; terminated != nil {
+				if terminated.ExitCode == 137 {
+
+					info := TerminatedPodInfo{
+						Pod:            pod,
+						ContainerName:  cStatus.Name,
+						StartTime:      terminated.StartedAt.String(),
+						TerminatedTime: terminated.FinishedAt.String(),
+					}
+
+					terminatedPodsInfo = append(terminatedPodsInfo, info)
+				}
+			}
+		}
+	}
+
+	for _, v := range terminatedPodsInfo {
+		fmt.Println(v.ContainerName, v.Pod.Name)
 	}
 
 	return nil
