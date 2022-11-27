a5e645f0c46d4f520b06822bf561a2c44fd468c3
Jack Dockerty <jdockerty19@gmail.com>
2022-11-27 16:49:49 +0000

    add util func for retrieval of k8s client/config

--- pkg/plugin/plugin.go
+++ pkg/plugin/plugin.go
@@ -8,6 +8,7 @@ import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/cli-runtime/pkg/genericclioptions"
 	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/rest"
 )
 
 // TerminatedPodInfo is a wrapper struct around an OOMKilled Pod's information.
@@ -23,6 +24,21 @@ type TerminatedPodInfo struct {
 type MemoryInfo struct {
 	Request string
 	Limit   string
+}
+
+func getK8sClientAndConfig(configFlags *genericclioptions.ConfigFlags) (*kubernetes.Clientset, *rest.Config, error) {
+
+	config, err := configFlags.ToRESTConfig()
+	if err != nil {
+		return nil, nil, fmt.Errorf("failed to read kubeconfig: %w", err)
+	}
+
+	clientset, err := kubernetes.NewForConfig(config)
+	if err != nil {
+		return nil, nil, fmt.Errorf("failed to create clientset: %w", err)
+	}
+
+	return clientset, config, nil
 }
 
 // GetNamespace will retrieve the current namespace from the provided namespace or kubeconfig file of the caller
@@ -112,14 +128,10 @@ func BuildTerminatedPodsInfo(client *kubernetes.Clientset, namespace string) ([]
 
 // Run returns the pod information for those that have been OOMKilled, this provides the plugin functionality.
 func Run(configFlags *genericclioptions.ConfigFlags, namespace string) ([]TerminatedPodInfo, error) {
-	config, err := configFlags.ToRESTConfig()
-	if err != nil {
-		return nil, fmt.Errorf("failed to read kubeconfig: %w", err)
-	}
 
-	clientset, err := kubernetes.NewForConfig(config)
+	clientset, _, err := getK8sClientAndConfig(configFlags)
 	if err != nil {
-		return nil, fmt.Errorf("failed to create clientset: %w", err)
+		return nil, fmt.Errorf("unable to get Kubernetes client and config: %s", err)
 	}
 
 	terminatedPods, err := BuildTerminatedPodsInfo(clientset, namespace)
